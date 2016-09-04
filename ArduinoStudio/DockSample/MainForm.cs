4a66afd8ede35c6bc2d6ccff633e55f5b5481f48
Thijs Elenbaas <thijs@contemplated.nl>
2016-09-04 17:23:54 +0200

    Basic compiler output in single window

--- ArduinoStudio/DockSample/MainForm.cs
+++ ArduinoStudio/DockSample/MainForm.cs
@@ -29,13 +29,11 @@ namespace ArduinoStudio
 
             _deserializeDockContent = GetContentFromPersistString;
 
-            //    vS2012ToolStripExtender1.DefaultRenderer = _toolStripProfessionalRenderer;
-            //    vS2012ToolStripExtender1.VS2012Renderer = _vs2012ToolStripRenderer;
+
             vS2012ToolStripExtender1.VS2013Renderer = _vs2013ToolStripRenderer;
 
             topBar.BackColor = bottomBar.BackColor = Color.FromArgb(0xFF, 41, 57, 85);
 
-            //SetSchema(this.menuItemSchemaVS2013Blue, null);
             SetSchema();
         }
 
@@ -46,7 +44,7 @@ namespace ArduinoStudio
 
         #region Methods
 
-        private IDockContent FindDocument(string text)
+        private IDockContent FindSketch(string text)
         {
             if (dockPanel.DocumentStyle == DocumentStyle.SystemMdi)
             {
@@ -63,13 +61,13 @@ namespace ArduinoStudio
             return null;
         }
 
-        private Document CreateNewDocument()
+        private DocumentWindow CreateNewDocument()
         {
-            var dummyDoc = new Document();
+            var dummyDoc = new DocumentWindow();
 
             var count = 1;
             var text = "Document" + count;
-            while (FindDocument(text) != null)
+            while (FindSketch(text) != null)
             {
                 count++;
                 text = "Document" + count;
@@ -78,9 +76,9 @@ namespace ArduinoStudio
             return dummyDoc;
         }
 
-        private Document CreateNewDocument(string text)
+        private DocumentWindow CreateNewDocument(string text)
         {
-            var dummyDoc = new Document();
+            var dummyDoc = new DocumentWindow();
             dummyDoc.Text = text;
             return dummyDoc;
         }
@@ -120,10 +118,10 @@ namespace ArduinoStudio
             if (parsedStrings.Length != 3)
                 return null;
 
-            if (parsedStrings[0] != typeof(Document).ToString())
+            if (parsedStrings[0] != typeof(DocumentWindow).ToString())
                 return null;
 
-            var dummyDoc = new Document();
+            var dummyDoc = new DocumentWindow();
             if (parsedStrings[1] != string.Empty)
                 dummyDoc.FileName = parsedStrings[1];
             if (parsedStrings[2] != string.Empty)
@@ -145,8 +143,7 @@ namespace ArduinoStudio
             CloseAllDocuments();
         }
 
-        //    private readonly ToolStripRenderer _toolStripProfessionalRenderer = new ToolStripProfessionalRenderer();
-        //    private readonly ToolStripRenderer _vs2012ToolStripRenderer = new VS2012ToolStripRenderer();
+
         private readonly ToolStripRenderer _vs2013ToolStripRenderer = new Vs2013ToolStripRenderer();
 
 
@@ -156,30 +153,6 @@ namespace ArduinoStudio
             EnableVSRenderer(VSToolStripExtender.VsVersion.Vs2013);
         }
 
-        //private void SetSchema(object sender, System.EventArgs e)
-        //{
-        //    // Persist settings when rebuilding UI
-        //    string configFile = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "DockPanel.temp.config");
-
-        //    dockPanel.SaveAsXml(configFile);
-        //    CloseAllContents();
-
-
-        //    if (sender == this.menuItemSchemaVS2013Blue)
-        //    {
-        //        this.dockPanel.Theme = this.vS2013BlueTheme1;
-        //        this.EnableVSRenderer(VSToolStripExtender.VsVersion.Vs2013);
-        //    }
-
-        //    menuItemSchemaVS2005.Checked = (sender == menuItemSchemaVS2005);
-        //    menuItemSchemaVS2003.Checked = (sender == menuItemSchemaVS2003);
-        //    menuItemSchemaVS2012Light.Checked = (sender == menuItemSchemaVS2012Light);
-        //    this.menuItemSchemaVS2013Blue.Checked = (sender == this.menuItemSchemaVS2013Blue);
-        //    this.topBar.Visible = this.bottomBar.Visible = (sender == this.menuItemSchemaVS2013Blue);
-
-        //    if (File.Exists(configFile))
-        //        dockPanel.LoadFromXml(configFile, _deserializeDockContent);
-        //}
 
         private void EnableVSRenderer(VSToolStripExtender.VsVersion version)
         {
@@ -188,86 +161,7 @@ namespace ArduinoStudio
             vS2012ToolStripExtender1.SetStyle(statusBar, version);
         }
 
-        //private void SetDocumentStyle(object sender, System.EventArgs e)
-        //{
-        //    DocumentStyle oldStyle = dockPanel.DocumentStyle;
-        //    DocumentStyle newStyle;
-        //    if (sender == menuItemDockingMdi)
-        //        newStyle = DocumentStyle.DockingMdi;
-        //    else if (sender == menuItemDockingWindow)
-        //        newStyle = DocumentStyle.DockingWindow;
-        //    else if (sender == menuItemDockingSdi)
-        //        newStyle = DocumentStyle.DockingSdi;
-        //    else
-        //        newStyle = DocumentStyle.SystemMdi;
-
-        //    if (oldStyle == newStyle)
-        //        return;
-
-        //    if (oldStyle == DocumentStyle.SystemMdi || newStyle == DocumentStyle.SystemMdi)
-        //        CloseAllDocuments();
-
-        //    dockPanel.DocumentStyle = newStyle;
-        //    menuItemLayoutByCode.Enabled = (newStyle != DocumentStyle.SystemMdi);
-        //    menuItemLayoutByXml.Enabled = (newStyle != DocumentStyle.SystemMdi);
-
-        //}
-
-        //private AutoHideStripSkin _autoHideStripSkin;
-        //private DockPaneStripSkin _dockPaneStripSkin;
-
-        //private void SetDockPanelSkinOptions(bool isChecked)
-        //{
-        //    if (isChecked)
-        //    {
-        //        // All of these options may be set in the designer.
-        //        // This is not a complete list of possible options available in the skin.
-
-        //        var autoHideSkin = new AutoHideStripSkin();
-        //        autoHideSkin.DockStripGradient.StartColor = Color.AliceBlue;
-        //        autoHideSkin.DockStripGradient.EndColor = Color.Blue;
-        //        autoHideSkin.DockStripGradient.LinearGradientMode = LinearGradientMode.ForwardDiagonal;
-        //        autoHideSkin.TabGradient.StartColor = SystemColors.Control;
-        //        autoHideSkin.TabGradient.EndColor = SystemColors.ControlDark;
-        //        autoHideSkin.TabGradient.TextColor = SystemColors.ControlText;
-        //        autoHideSkin.TextFont = new Font("Showcard Gothic", 10);
-
-        //        _autoHideStripSkin = dockPanel.Skin.AutoHideStripSkin;
-        //        dockPanel.Skin.AutoHideStripSkin = autoHideSkin;
-
-        //        var dockPaneSkin = new DockPaneStripSkin();
-        //        dockPaneSkin.DocumentGradient.DockStripGradient.StartColor = Color.Red;
-        //        dockPaneSkin.DocumentGradient.DockStripGradient.EndColor = Color.Pink;
-
-        //        dockPaneSkin.DocumentGradient.ActiveTabGradient.StartColor = Color.Green;
-        //        dockPaneSkin.DocumentGradient.ActiveTabGradient.EndColor = Color.Green;
-        //        dockPaneSkin.DocumentGradient.ActiveTabGradient.TextColor = Color.White;
-
-        //        dockPaneSkin.DocumentGradient.InactiveTabGradient.StartColor = Color.Gray;
-        //        dockPaneSkin.DocumentGradient.InactiveTabGradient.EndColor = Color.Gray;
-        //        dockPaneSkin.DocumentGradient.InactiveTabGradient.TextColor = Color.Black;
-
-        //        dockPaneSkin.TextFont = new Font("SketchFlow Print", 10);
-
-        //        _dockPaneStripSkin = dockPanel.Skin.DockPaneStripSkin;
-        //        dockPanel.Skin.DockPaneStripSkin = dockPaneSkin;
-        //    }
-        //    else
-        //    {
-        //        if (_autoHideStripSkin != null)
-        //        {
-        //            dockPanel.Skin.AutoHideStripSkin = _autoHideStripSkin;
-        //        }
-
-        //        if (_dockPaneStripSkin != null)
-        //        {
-        //            dockPanel.Skin.DockPaneStripSkin = _dockPaneStripSkin;
-        //        }
-        //    }
-
-        //    menuItemLayoutByXml_Click(menuItemLayoutByXml, EventArgs.Empty);
-        //}
-
+   
         #endregion
 
         #region Event Handlers
@@ -325,7 +219,7 @@ namespace ArduinoStudio
             var openFile = new OpenFileDialog();
 
             openFile.InitialDirectory = Application.ExecutablePath;
-            openFile.Filter = "rtf files (*.rtf)|*.rtf|txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFile.Filter = "Arduino sketches (*.ino)|*.rtf|Arduino sketches pre-1.0 (*.pde)|*.txt|All files (*.*)|*.*";
             openFile.FilterIndex = 1;
             openFile.RestoreDirectory = true;
 
@@ -334,27 +228,32 @@ namespace ArduinoStudio
                 var fullName = openFile.FileName;
                 var fileName = Path.GetFileName(fullName);
 
-                if (FindDocument(fileName) != null)
+                // todo management of multiple sketches should not happen in UI
+                if (FindSketch(fileName) != null)
                 {
-                    MessageBox.Show("The document: " + fileName + " has already opened!");
+                    MessageBox.Show("The sketch: " + fileName + " has already been opened!");
                     return;
                 }
 
-                var dummyDoc = new Document {Text = fileName};
-                if (dockPanel.DocumentStyle == DocumentStyle.SystemMdi)
-                {
-                    dummyDoc.MdiParent = this;
-                    dummyDoc.Show();
-                }
-                else
-                    dummyDoc.Show(dockPanel);
+                // Create compiler environment
+                //var _arduinoEnvironments = new ArduinoEnvironments();
+                //if (_arduinoEnvironments.ArduinoIdeList.Count > 0)
+                //{
+                //    _arduinoEnvironment = _arduinoEnvironments.ArduinoIdeList[0];
+                //    _arduinoEnvironment.OutputHandler += ArduinoEnvironmentOutput;
+                //}
+
+                // Create window
+                var compileUploadWindow = new CompileUploadWindow() {Text = fileName};
+                compileUploadWindow.Show(dockPanel);
                 try
                 {
-                    dummyDoc.FileName = fullName;
+                    // Try interaction with window. May already been closed?
+                    //dummyDoc.FileName = fullName;
                 }
                 catch (Exception exception)
                 {
-                    dummyDoc.Close();
+                    compileUploadWindow.Close();
                     MessageBox.Show(exception.Message);
                 }
             }
@@ -441,15 +340,6 @@ namespace ArduinoStudio
             newWindow.Show();
         }
 
-        //private void menuItemTools_Popup(object sender, System.EventArgs e)
-        //{
-        //    menuItemLockLayout.Checked = !this.dockPanel.AllowEndUserDocking;
-        //}
-
-        //private void menuItemLockLayout_Click(object sender, System.EventArgs e)
-        //{
-        //    dockPanel.AllowEndUserDocking = !dockPanel.AllowEndUserDocking;
-        //}
 
         private void MenuItemLayoutByCodeClick(object sender, EventArgs e)
         {
@@ -561,34 +451,6 @@ namespace ArduinoStudio
             }
         }
 
-        //private void menuItemShowDocumentIcon_Click(object sender, System.EventArgs e)
-        //{
-        //    dockPanel.ShowDocumentIcon = menuItemShowDocumentIcon.Checked = !menuItemShowDocumentIcon.Checked;
-        //}
-
-        //private void showRightToLeft_Click(object sender, EventArgs e)
-        //{
-        //    CloseAllContents();
-        //    if (showRightToLeft.Checked)
-        //    {
-        //        this.RightToLeft = RightToLeft.No;
-        //        this.RightToLeftLayout = false;
-        //    }
-        //    else
-        //    {
-        //        this.RightToLeft = RightToLeft.Yes;
-        //        this.RightToLeftLayout = true;
-        //    }
-        //    _solutionExplorer.RightToLeftLayout = this.RightToLeftLayout;
-        //    showRightToLeft.Checked = !showRightToLeft.Checked;
-        //}
-
-        //private void ExitWithoutSavingLayoutClick(object sender, EventArgs e)
-        //{
-        //    _bSaveLayout = false;
-        //    Close();
-        //    _bSaveLayout = true;
-        //}
 
         #endregion
     }
